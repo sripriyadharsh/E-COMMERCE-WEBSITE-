e558768ad563114e0fa9704d247b508311ad602a
sri <131853932+sripriyadharsh@users.noreply.github.com>
2023-10-29 22:20:56 +0530

    Add files via upload

---
+++ Task-2/App.js
@@ -0,0 +1,60 @@
+
+import './App.css';
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import Home from './components/Home';
+import { Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {HelmetProvider} from 'react-helmet-async';
+import {ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ProductDetail from './components/product/ProductDetail';
+import ProductSearch from './components/product/ProductSearch';
+import Login from './components/user/Login';
+import Register from './components/user/Register';
+import { useEffect } from 'react';
+import store from './store';
+import { loadUser } from './actions/userActions';
+import Profile from './components/user/Profile';
+import ProtectedRoute from './components/route/ProtectedRoute';
+import UpdateProfile from './components/user/UpdateProfile';
+import UpdatePassword from './components/user/UpdatePassword';
+import ForgotPassword from './components/user/ForgotPassword';
+import ResetPassword from './components/user/ResetPassword';
+
+
+function App() {
+
+  useEffect(()=>{
+    store.dispatch(loadUser)
+  })
+  return (
+    <Router>
+    <div className="App">
+      <HelmetProvider>
+        <Header/>
+        <div className='container container-fluid'>
+          <ToastContainer theme='dark'/>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/search/:keyword' element={<ProductSearch/>}/>
+              <Route path='/product/:id' element={<ProductDetail/>}/>
+              <Route path='/Login' element={<Login/>}/>
+              <Route path='/Register' element={<Register/>}/>
+              <Route path='/myprofile' element={<ProtectedRoute><Profile/></ProtectedRoute>}/>
+              <Route path='/myprofile/update' element={<ProtectedRoute><UpdateProfile/></ProtectedRoute>}/>
+              <Route path='/myprofile/update/password' element={<ProtectedRoute><UpdatePassword/></ProtectedRoute>}/> 
+              <Route path='/password/forgot' element={<ForgotPassword/>}/> 
+              <Route path='/password/reset/:token' element={<ResetPassword/>}/>               
+            </Routes>
+        </div>
+
+        
+        
+        <Footer/>
+      </HelmetProvider>
+         </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Task-2/userActions.js
@@ -0,0 +1,144 @@
+import {
+    loginFail, 
+    loginRequest, 
+    loginSuccess, 
+    clearError,
+    registerFail,
+    registerRequest, 
+    registerSuccess,
+    loadUserFail,
+    loadUserRequest,
+    loadUserSuccess,
+    logoutFail,
+    logoutSuccess,
+    updateProfileFail,
+    updateProfileRequest,
+    updateProfileSuccess,
+    updatePasswordFail,
+    updatePasswordRequest,
+    updatePasswordSuccess,
+    forgotPasswordRequest,
+    forgotPasswordSuccess,
+    forgotPasswordFail,
+    resetPasswordRequest,
+    resetPasswordSuccess,
+    resetPasswordFail
+} from '../slices/authSlice';
+import axios from 'axios';
+
+export const login=(email, password)=> async (dispatch)=>{
+
+    try{
+        dispatch(loginRequest())
+        const {data} =await axios.post(`/api/v1/login`,{email,password});
+        dispatch(loginSuccess(data))
+    }catch(error){
+        dispatch(loginFail(error.response.data.message))
+    }
+}
+
+export const clearAuthError = dispatch=>{
+    dispatch(clearError())
+}
+
+export const register=(userData)=> async (dispatch)=>{
+
+    try{
+        dispatch(registerRequest())
+        const config ={
+            headers:{
+                'content-type': 'multipart/form-data'
+            }
+        }
+        const { data } =await axios.post(`/api/v1/register`,userData, config);
+        dispatch(registerSuccess(data))
+    }catch(error){
+        dispatch(registerFail(error.response.data.message))
+    }
+}
+
+export const loadUser= async (dispatch)=>{
+
+    try{
+        dispatch(loadUserRequest())
+        
+        const { data } =await axios.get(`/api/v1/myprofile`);
+        dispatch(loadUserSuccess(data))
+    }catch(error){
+        dispatch(loadUserFail(error.response.data.message))
+    }
+}
+
+export const logout= async (dispatch)=>{
+
+    try{
+        await axios.get(`/api/v1/logout`);
+        dispatch(logoutSuccess())
+    }catch(error){
+        dispatch(logoutFail)
+    }
+}
+
+export const updateProfile=(userData)=> async (dispatch)=>{
+
+    try{
+        dispatch(updateProfileRequest())
+        const config ={
+            headers:{
+                'content-type': 'multipart/form-data'
+            }
+        }
+        const { data } =await axios.put(`/api/v1/update`,userData, config);
+        dispatch(updateProfileSuccess(data))
+    }catch(error){
+        dispatch(updateProfileFail(error.response.data.message))
+    }
+}
+
+export const updatePassword=(formData)=> async (dispatch)=>{
+
+    try{
+        dispatch(updatePasswordRequest())
+        const config ={
+            headers:{
+                'content-type': 'application/json'
+            }
+        }
+        await axios.put(`/api/v1/password/change`, formData, config);
+        dispatch(updatePasswordSuccess())
+    }catch(error){
+        dispatch(updatePasswordFail(error.response.data.message))
+    }
+}
+
+export const forgotPassword=(formData)=> async (dispatch)=>{
+
+    try{
+        dispatch(forgotPasswordRequest())
+        const config ={
+            headers:{
+                'content-type': 'application/json'
+            }
+        }
+        const {data}=await axios.post(`/api/v1/password/forgot`, formData, config);
+        dispatch(forgotPasswordSuccess(data))
+    }catch(error){
+        dispatch(forgotPasswordFail(error.response.data.message))
+    }
+}
+
+export const resetPassword=(formData, token)=> async (dispatch)=>{
+
+    try{
+        dispatch(resetPasswordRequest())
+        const config ={
+            headers:{
+                'content-type': 'application/json'
+            }
+        }
+        const {data}=await axios.post(`/api/v1/password/reset/${token}`, formData, config);
+        dispatch(resetPasswordSuccess(data))
+    }catch(error){
+        dispatch(resetPasswordFail(error.response.data.message))
+    }
+}
